fc1c4c76a41754210cbf681f2dae1c9350be3509
ginger-kang <kdhoon07@gmail.com>
2021-09-21 18:18:08 +0900

    use the Link component

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About page</title>
+      <h1>About</h1>
+      <Link to="/">Back to Home</Link>
+    </main>
+  );
+};
+
+export default AboutPage;
