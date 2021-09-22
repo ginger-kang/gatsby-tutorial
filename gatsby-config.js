7a560d5ff9b77357e68f675a20e56dae8a2ac163
ginger-kang <kdhoon07@gmail.com>
2021-09-22 09:07:19 +0900

    create a blog page with a list of post filenames

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: "Gatsby tutorial",
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "blog",
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
